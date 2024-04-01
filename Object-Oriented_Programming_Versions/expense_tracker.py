982d2e056a562f1ca0692c55e7f4e8e3d00ed4e6
Code <idulsa.emmannicholasblabe.000@gmail.com>
2024-04-01 10:18:12 +0800

    layout foundation

---
+++ Object-Oriented_Programming_Versions/expense_tracker.py
@@ -0,0 +1,41 @@
+# EXPENSE TRACKER [OWN VERSION]
+
+class ExpenseTracker:
+
+    def __init__(self):
+        pass
+
+    # Add Expense Method
+    def addExpense(self):
+        pass
+
+    # Print List of Expenses Method
+    def listOfExpenses(self):
+        pass
+
+    # Print Total Expenses Method
+    def totalExpenses(self):
+        pass
+
+    # Filter Expenses by Category Method
+    def filterExpenses(self):
+        pass
+
+    # Delete Expense Method
+    def deleteExpense(self):
+        pass
+
+    # Clear Expenses Method
+    def clearExpenses(self):
+        pass
+
+    # Run Method    
+    def run(self):
+        pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
